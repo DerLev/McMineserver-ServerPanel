4188be1d532b02dff8145d0aee19e939e9e26627
DerLev <33765680+DerLev@users.noreply.github.com>
2021-03-13 17:31:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ echo -e "\nMounting to server directory"
 mkdir -p /mnt/server >/dev/null 2>&1
 cd /mnt/server >/dev/null 2>&1
 echo -e "Pulling README.txt"
-curl -o README.txt https://raw.githubusercontent.com/DerLev/McMineserver-ServerPanel/81328e91020102dc864aa663a7b83186fe67fa19/README.txt >/dev/null 2>&1
+curl -o README.txt https://raw.githubusercontent.com/DerLev/McMineserver-ServerPanel/bd2587ccda4d6c20cde8eb1fec5eaa515b90e3a8/README.txt >/dev/null 2>&1
+echo -e "Pulling .pteroignore"
+curl -o .pteroignore https://raw.githubusercontent.com/DerLev/McMineserver-ServerPanel/bd2587ccda4d6c20cde8eb1fec5eaa515b90e3a8/.pteroignore >/dev/null 2>&1
 echo -e "\n\n=== Install Complete ==="
